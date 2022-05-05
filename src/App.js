ec7a75afd1255730f03726a26e1b0d0889359ccb
WaruiAlfred <alfredwarui17@gmail.com>
2022-05-05 10:19:16 +0300

    install and configure styled components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./components/styled/Global";
+
+const theme = {}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+      <p>Birthday Reminder</p>
+    </ThemeProvider>
   );
 }
 
