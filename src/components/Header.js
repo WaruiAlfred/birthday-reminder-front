0b1c4eab8a1f7f488e020bdb1213e555194f69dc
WaruiAlfred <alfredwarui17@gmail.com>
2022-05-10 13:38:25 +0300

    style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import { StyledHeader } from "./styled/Header.styled";
+import img from "../assets/img/birthday.jpg";
 
 function Header() {
   return (
     <StyledHeader>
       <h1>Birthday Reminder</h1>
+      <img src={img} alt="party img" />
     </StyledHeader>
   );
 }
--- src/components/styled/Header.styled.js
+++ src/components/styled/Header.styled.js
@@ -6,10 +6,16 @@ export const StyledHeader = styled.header`
 
   display: flex;
   align-items: center;
+  justify-content: space-between;
 
-  h1{
-    font-family: 'Lobster', cursive;
-    color: #fff;
+  h1 {
+    font-family: "Lobster", cursive;
     margin-left: 3rem;
+  }
+
+  img {
+    height: 100%;
+    width: 20rem;
+    clip-path: polygon(48% 53%, 100% 0, 100% 100%, 0 100%);
   }
 `;
