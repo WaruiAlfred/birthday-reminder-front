dbe607df25f2a67bb0e58637593f4876e2815d34
WaruiAlfred <alfredwarui17@gmail.com>
2022-05-10 23:47:58 +0300

    render all forms for development

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,11 +1,17 @@
 import { MainContent, BodyImg } from "../styled/content/Content.styled";
-import Details from "./sub-contents/Details";
+// import Details from "./sub-contents/Details";
+import BirthdayEntryForm from "../forms/BirthdayEntryForm";
+import LoginForm from "../forms/LoginForm";
+import RegisterForm from "../forms/RegisterForm";
 
 function Content() {
   return (
     <MainContent>
       <BodyImg />
-      <Details/>
+      {/*<Details/>*/}
+      <BirthdayEntryForm />
+      <LoginForm />
+      <RegisterForm />
     </MainContent>
   );
 }
