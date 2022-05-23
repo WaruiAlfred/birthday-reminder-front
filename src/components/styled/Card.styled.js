8a01e3cf8bca2d5776469b6b26713db5eb6d8dbe
WaruiAlfred <alfredwarui17@gmail.com>
2022-05-23 13:22:23 +0300

    create a custom card

---
+++ src/components/styled/Card.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  padding: 1.5rem;
+  margin: 0.6rem 0;
+  border-radius: 2rem;
+  box-shadow: 1rem 1rem 1rem ${({ theme }) => theme.colors.grey_light_2};
+  width: ${({ width }) => width || "24rem"};
+  height: ${({ height }) => height || "10rem"};
+  font-size: ${({ font }) => font || "1.5rem"};
+`;
+
+export default Card;
