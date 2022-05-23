aba18fc72f5b3b363beb4efd07b998085bd45ec8
WaruiAlfred <alfredwarui17@gmail.com>
2022-05-23 13:22:40 +0300

    create homepage

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import { StyledHomeContainer, Description, Join } from "./styled/Home.styled";
+import Card from "./styled/Card.styled";
+import NewUser from "./content/sub-contents/user/NewUser";
+
+function Home() {
+  return (
+    <StyledHomeContainer>
+      <Description>
+        <p>
+          Ever found yourself forgetting your dear ones birthdays? Here we've
+          come up with a solution where all you have to do is enter their
+          birthdates and we'll do the remembering for you.
+        </p>
+        <h5>Here are services we offer:</h5>
+        <div>
+          <Card>Keep records of birthdays</Card>
+          <Card>Send you a reminder email when the birthday arrives</Card>
+          <Card>Send suggestions of what to do</Card>
+
+          <Card>Keep records of user data including emails</Card>
+          <Card>Provide support if needed</Card>
+          <Card>Lessen the hassle of having to remember birthdays</Card>
+        </div>
+      </Description>
+      <Join>
+        <NewUser/>
+      </Join>
+    </StyledHomeContainer>
+  );
+}
+
+export default Home;
---
+++ src/components/styled/Home.styled.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const StyledHomeContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+`;
+
+export const Description = styled.div`
+  padding: 2rem;
+
+  p {
+    text-align: center;
+    margin: 1rem 0;
+    font-weight: 300;
+    font-size: 1.8rem;
+  }
+
+  h5 {
+    text-align: center;
+    margin-top: 5rem;
+  }
+
+  div {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+`;
+
+export const Join = styled.div`
+  align-self: center;
+`;
