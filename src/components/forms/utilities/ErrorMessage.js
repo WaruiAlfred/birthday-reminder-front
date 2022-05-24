96f471f43fab0c26a98767d9aa1761ede9605685
WaruiAlfred <alfredwarui17@gmail.com>
2022-05-24 16:48:35 +0300

    create error message component

---
+++ src/components/forms/utilities/ErrorMessage.js
@@ -0,0 +1,7 @@
+import { ErrorMessageDiv } from "../../styled/forms/Form.styled";
+
+const ErrorMessage = ({ children }) => {
+  return <ErrorMessageDiv>{children}</ErrorMessageDiv>;
+};
+
+export default ErrorMessage;
