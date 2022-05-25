dfcbe803a5a421ea16f7871b81e951a9ede7584b
WaruiAlfred <alfredwarui17@gmail.com>
2022-05-26 02:27:08 +0300

    provide app context to the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { ThemeProvider } from "styled-components";
 import Container from "./components/Container";
 import GlobalStyles from "./components/styled/Global";
+import AppContextProvider from "./store/appContext";
 import { theme } from "./theme";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles/>
-      <Container/>
-    </ThemeProvider>
+    <AppContextProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Container />
+      </ThemeProvider>
+    </AppContextProvider>
   );
 }
 
