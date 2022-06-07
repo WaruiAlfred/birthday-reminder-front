6a3e97b6a78c1d0e8340e3106a166633c94ec492
WaruiAlfred <alfredwarui17@gmail.com>
2022-06-07 19:36:40 +0300

    create and style Birthdays component

--- src/components/content/sub-contents/user/birthdays/Birthdays.js
+++ src/components/content/sub-contents/user/birthdays/Birthdays.js
@@ -1,13 +1,18 @@
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
 import { useHttp } from "../../../../../hooks/use-http";
 import { Wrapper } from "../../../../styled/content/sub-contents/user/birthdays/Birthdays.styled";
 import NoBirthdays from "./NoBirthdays";
 import { FadeLoadingSpinner } from "../../../../forms/utilities/LoadingSpinner";
 import UserWelcome from "../UserWelcome";
+import moment from "moment";
+import { StyledButton } from "../../../../styled/forms/Button.styled";
+import { StyledAuthLink } from "../../../../styled/content/sub-contents/user/User.styled";
+import ErrorMessage from "../../../../forms/utilities/ErrorMessage";
 
 const Birthdays = () => {
   const { sendRequest, loading, data, error } = useHttp();
   let noData = false;
+  let content = <NoBirthdays />;
 
   useEffect(() => {
     const getBirthdays = () => {
@@ -24,6 +29,40 @@ const Birthdays = () => {
     noData = true;
   }
 
+  if (data) {
+    content = (
+      <Fragment>
+        <p>These are your dear ones' birthdays</p>
+        <div className="birthdays">
+          {data.map((birthday, i) => (
+            <div key={i} className="birthday">
+              <div className="birthday__header">
+                <p>{birthday.friends_name}</p>
+                <p>
+                  {moment(
+                    new Date(
+                      birthday.year_of_birth,
+                      birthday.month_of_birth,
+                      birthday.day_of_birth
+                    )
+                  ).format("L")}
+                </p>
+              </div>
+              <p>Due</p>
+            </div>
+          ))}
+          <div className="buttons">
+            <StyledButton>
+              <StyledAuthLink to="/addBirthday">Add </StyledAuthLink>
+            </StyledButton>
+            <StyledButton>More &rarr;</StyledButton>
+          </div>
+        </div>
+        {error && <ErrorMessage>{error}</ErrorMessage>}
+      </Fragment>
+    );
+  }
+
   return (
     <Wrapper>
       <UserWelcome />
@@ -31,6 +70,7 @@ const Birthdays = () => {
         {loading && !data && <FadeLoadingSpinner loading={loading} />}
       </div>
       {noData && <NoBirthdays />}
+      <div className="content">{content}</div>
     </Wrapper>
   );
 };
--- src/components/styled/content/sub-contents/user/birthdays/Birthdays.styled.js
+++ src/components/styled/content/sub-contents/user/birthdays/Birthdays.styled.js
@@ -1,8 +1,55 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  .loading-spinner{
+  font-weight: 300;
+
+  .loading-spinner {
     width: 9rem;
     margin: 0 auto;
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
+  }
+
+  .content > p {
+    text-align: center;
+    margin: 2rem 0;
+  }
+
+  .birthdays {
+    background-color: ${({ theme }) => theme.content_bg};
+    border-radius: 1rem;
+    padding: 3rem;
+    margin-bottom: 2rem;
+
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(40rem, 1fr));
+    gap: 1rem;
+  }
+
+  .birthday {
+    background-color: #fff;
+    border-radius: 0.9rem;
+    padding: 2rem;
+    max-width: 70rem;
+
+    &__header{
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+
+  .buttons {
+    display: flex;
+    justify-content: space-between;
+    padding: 0 2rem;
+
+    @media screen and (min-width: 55em) {
+      grid-column: 2 / 3;
+    }
   }
 `;
